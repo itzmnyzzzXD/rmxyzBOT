8c3e8b034cccba8d214769a811a5a4fe7585e5de
itzmnyzzzXD <mua35351@gmail.com>
2026-09-13 21:59:55 +0300

    Route login to dedicated auth screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import LoginPage from './LoginPage'
 import './styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(<React.StrictMode>{window.location.pathname === '/login' ? <LoginPage /> : <App />}</React.StrictMode>)
