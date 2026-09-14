95f7ac7ac990a1c44e2c58eef1635da13197fb58
itzmnyzzzXD <mua35351@gmail.com>
2026-09-14 03:50:40 +0300

    Load RM visual system stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './AppV2'
 import './styles.css'
+import './rm.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(<React.StrictMode><App /></React.StrictMode>)
